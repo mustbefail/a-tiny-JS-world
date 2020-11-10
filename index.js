4def90e21fcf494dbc5dc21f3d5ba99fafed502b
Viktor Kushnir <mustbefail@gmail.com>
2020-11-10 18:07:38 +0200

    Array.toString implimentation

--- index.js
+++ index.js
@@ -7,52 +7,70 @@
 
 // ======== OBJECTS DEFINITIONS ========
 // Define your objects here
-const cat = {
-  species: 'cat',
-  name: 'Prokhor',
-  hands: 0,
-  legs: 4,
-  saying: 'Meow!',
-  gender: 'male',
-  friends: 'June, Maki',
-};
-const catWoman = {
-  species: 'cat',
-  name: 'June',
-  hands: 0,
-  legs: 4,
-  saying: cat.saying,
-  gender: 'female',
-  friends: 'Anna',
-};
-const dog = {
-  species: 'dog',
-  name: 'Maki',
-  hands: 0,
-  legs: 4,
-  saying: 'bark-bark!',
-  gender: 'male',
-  friends: 'Alfred, Prokhor, Anna',
-};
-const man = {
-  species: 'human',
-  name: 'Alfred',
-  hands: 2,
-  legs: 2,
-  saying: 'Master Bruce, I...',
-  gender: 'male',
-  friends: 'Maki, Anna',
-};
-const woman = {
-  species: 'human',
-  name: 'Anna',
-  hands: 2,
-  legs: 2,
-  saying: 'Need more money!',
-  gender: 'female',
-  friends: 'June, Alfred',
-};
+class Inhabitant {
+  constructor(species, name, gender, phrase, friends, legs) {
+    this.friends = friends ? friends : ['no friends'];
+    this.species = species;
+    this.name = name;
+    this.gender = gender;
+    this.legs = legs;
+    this.phrase = phrase;
+  }
 
+  saying() {
+    return this.phrase;
+  }
+
+  toString() {
+    return `Hello! I am <strong>${this.species}</strong>, my name is <strong>${
+      this.name
+    }</strong>, my gender is <strong>${
+      this.gender
+    }</strong>,My friends: <strong>${
+      this.friends
+    }</strong>  and you know what: <strong>${this.saying()}</strong>`;
+  }
+}
+
+class Cat extends Inhabitant {
+  constructor(name, gender, phrase, friends, legs = 4) {
+    super('cat', name, gender, phrase, friends, legs);
+  }
+
+  toString() {
+    return super.toString() + ` I have <strong>${this.legs}</strong> legs`;
+  }
+}
+
+class Dog extends Inhabitant {
+  constructor(name, gender, phrase, friends, legs = 4) {
+    super('dog', name, gender, phrase, friends, legs);
+  }
+  toString() {
+    return super.toString() + ` I have <strong>${this.legs}</strong> legs`;
+  }
+}
+
+class Human extends Inhabitant {
+  constructor(name, gender, phrase, friends, hands = 2, legs = 2) {
+    super('human', name, gender, phrase, friends, legs);
+    this.hands = hands;
+  }
+  toString() {
+    return (
+      super.toString() + ` I have <strong>${this.hands}</strong> hands and <strong>${this.legs}</strong> legs`
+    );
+  }
+}
+
+const dog = new Dog('Maki', 'male', 'Bark-bark!');
+const cat = new Cat('Prokhor', 'male', 'Meow!');
+const man = new Human('Alfred', 'male', 'Master Bruce!');
+const woman = new Human('Anna', 'female', 'Need more money!');
+
+dog.friends = [cat.name, man.name];
+cat.friends = [woman.name, dog.name];
+man.friends = [woman.name, dog.name, cat.name, man.name];
 // ======== OUTPUT ========
 /* Use print(message) for output.
   Default tag for message is <pre>. Use print(message,'div') to change containing element tag.
@@ -62,21 +80,9 @@ const woman = {
   so code reviewers might focus on a single file that is index.js.
 */
 
-const inhabitants = [cat, catWoman, man, woman, dog];
+const inhabitants = [man, woman, dog, cat];
 
-const formatOutput = ({
-  name,
-  species,
-  hands,
-  legs,
-  saying,
-  gender,
-  friends,
-}) => {
-  return `Hello! I am ${species}, my name is ${name}, my gender is ${gender}, I have ${hands} arms and ${legs} legs and you know what: ${saying}. My friends: ${friends}`;
-};
-
-inhabitants.forEach((habitant) => print(formatOutput(habitant), 'h5'));
+inhabitants.forEach((habitant) => print(habitant));
 
 /* Print examples:
   print('ABC');
